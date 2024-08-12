6e753fe43280942f7106366480f0b5cff7075ef0
Erik Montgomery <115750399+erikemb@users.noreply.github.com>
2024-08-12 16:29:13 -0300

    Add files via upload

---
+++ gerenc_saldo.c
@@ -0,0 +1,45 @@
+
+void ativação_e_conf_do_display(){
+
+}
+
+void display_ativação(char numeros_a_ser_recebidos [] ){
+// recebe um vetor do tipo [9,9,-,2]. Os dois primeiros dados representamo saldo e os dois ultimos o numero 
+//aleatorio gerado
+// Aqui vai fazer a exibição do display.
+}
+
+capitação_de_voz(){
+
+}
+
+
+int gerador_de_numero_aleatorio(int num_tentativa ){
+    //gera um numero aleatório que varia de -9 a 9.
+    //recebe o numero de tentativa que é um variavel global e abaixo de 5 tentativas
+    // a probabilidade de aparecer um número negativo é de 50% 
+    // e o tempo de permanência de cada número é de 250 ms.
+    //A partir da quinta jogada, a probabilidade de um número negativo aparcer 
+    //é 70% e o tempo de permanência do negativo é de 250 ms enquanto o positivo é de 150 ms
+}
+
+int gerenciador_de_saldo(int num_aleatorio,int saldo_altual, int opcao){
+// o saldo pode variar de 00 a 99
+// esse bloco incrementa e decrementa esse valor
+// se opção for 0 o usuário pede crédito, se 1 o usuário quer pegar o numéro_aleatorio
+// Se usuário for "Crédito", um credito de 30 é adicionado
+}
+
+void conexão_wifi(){
+// bloco de código responsavel por fazer a conexão wifi
+}
+
+char gerenciador_de_exibição (int num_aleatorio, int saldo){
+
+}
+
+int main(){
+
+}
+
+
